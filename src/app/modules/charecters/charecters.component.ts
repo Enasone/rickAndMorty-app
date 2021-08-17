f2b996908ff033fce3fa6408960a15e39c69f2ee
Enasone <Usovdima977@gmail.com>
2021-08-18 01:31:23 +0500

    Refactor
    1. edit fun: next, prev and reset page

--- src/app/modules/charecters/charecters.component.ts
+++ src/app/modules/charecters/charecters.component.ts
@@ -17,10 +17,8 @@ export class CharectersComponent implements OnInit {
   names?: CharacterResult[]
   locations?: LocationResult[]
 
-  // species: any
-  // type: any
-  // genders: any
-  pagTest: number = 1
+
+  pages: number = 1
 
 
   constructor(
@@ -31,30 +29,10 @@ export class CharectersComponent implements OnInit {
   
 
   ngOnInit() {
-    // this.character.getCharacters().subscribe( res => {
-    //   this.characters = res.results
-
-          // this.names = res.results
-          // const spec = res.results.map( spec => spec.species)
-          // this.species = spec.filter( (item, pos) => {
-          //   return spec.indexOf(item) == pos;
-          // })
-
-          // const types = res.results.map( types => types.type)
-          // this.type = types.filter( (item, pos) => {
-          //   return types.indexOf(item) == pos && item != '' ;
-          // })
-
-          // const gender = res.results.map( genders => genders.gender)
-          // this.genders = gender.filter( (item, pos) => {
-          //   return gender.indexOf(item) == pos;
-          // })
-    // })
 
     this.route.queryParams.subscribe( (params: Params) => {
-      // console.log(params)
       if(params.page) {
-          this.pagTest = +params.page
+          this.pages = +params.page
           this.character.pages(+params.page).subscribe( res => {
           this.characters = res.results
         })
@@ -67,52 +45,16 @@ export class CharectersComponent implements OnInit {
     })
   }
 
-  // searchName(name: string ) {
-  //   this.character.getCharactersName(name).subscribe( res => {
-  //     this.characters = res.results
-  //   })
-  // }
-
   resetName() {
-
+    this.pages = 1
   }
 
   nextPage(number: number) {
-    
-    
-    this.route.queryParams.subscribe( (res: Params) => {
-      if(+res.page) {
-
-        this.pagTest = +res.page + 1
-      } else {
-
-        this.character.pages(++this.pagTest).subscribe( res => {
-          this.characters = res.results
-          this.names = res.results
-        })
-      }
-    })
+    this.pages += 1
   }
 
   prevPage(number: number) {
-    if (this.pagTest > 1) {
-
-      this.route.queryParams.subscribe( (res: Params) => {
-        if(+res.page) {
-
-          this.pagTest = +res.page - 1
-        } else {
-
-          this.character.pages(++this.pagTest).subscribe( res => {
-            this.characters = res.results
-            this.names = res.results
-          })
-        }
-      })
-
-    } else {
-      this.pagTest = 1
-    }
+    this.pages > 1? this.pages -=1 : this.pages = 1
   }
 }
 
